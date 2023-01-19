c1aa099273ac77d3abf558ac4f50ada04e596206
Mounssif Koumila <m.koumila@void.fr>
2023-01-19 17:53:47 +0100

    Add: creating a poc example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
 
-function App() {
+const Model = (props) => {
+  const { scene } = useGLTF("/car.glb");
+  return <primitive object={scene} {...props} />;
+};
+
+let App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas dpr={[1, 2]} camera={{ fov: 45 }} style={{ position: "absolute" }}>
+      <color attach="background" args={["#000"]} />
+      <PresentationControls speed={4} zoom={0.5} polar={[-0.1, Math.PI / 4]}>
+        <Stage environment={null}>
+          <Model scale={0.01} />
+        </Stage>
+      </PresentationControls>
+    </Canvas>
   );
-}
+};
 
 export default App;
